c2553ef52a65ea1f08a2e171ebfc4d3ccade2640
yasmin <yalharasis@pando-soft.com>
2026-04-04 01:08:01 +0300

    added Section header component already shared in all sections

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="about" id="about">
-    <SectionBadge :badgeTitle="badgeTitle" />
+    <SectionHeader :badgeTitle="badgeTitle" :subtitle="subtitle" :description="description"/>
   </section>
 </template>
 <script setup>
-import SectionBadge from "./Badge.vue";
+import SectionHeader from './SectionHeader.vue';
 const badgeTitle = "about me";
+const subtitle = "building digital solutions";
+const description = "passionate about creating exceptional user experiences through clean code and thoughtful design";
 </script>
---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <SectionBadge :badgeTitle="badgeTitle" />
+    <h2 class="subtitle">{{ subtitle }}</h2>
+    <p class="description">
+      {{ description }}
+    </p>
+  </header>
+</template>
+<script setup>
+import SectionBadge from "./Badge.vue";
+
+defineProps({
+  badgeTitle: String,
+  subtitle: String,
+  description: String,
+});
+</script>
+<style scoped lang="scss">
+@use "../assets/styles/_variables" as *;
+@use '../assets/styles/_media' as *;
+@use "../assets/styles/_mixins" as *;
+
+header {
+  @include flex-column;
+  width: inherit;
+  align-items: center;
+  gap: 15px;
+  .subtitle {
+    font-size: 2rem;
+    color: var(--color-text);
+  }
+  .description {
+    font-size: 1.3rem;
+    color: $secondary;
+    line-height: 1.6;
+    max-width: 750px;
+    text-align: center;
+    &::first-letter{
+        text-transform: capitalize;
+    }
+  }
+  // Tablet
+  @include tablet {
+    .subtitle {
+      font-size: 1.8rem;
+    }
+    .description {
+      font-size: 1.1rem;
+    }
+  }
+  // Mobile
+  @include mobile {
+    .subtitle {
+      font-size: 1.5rem;
+    }
+    .description {
+      font-size: 1rem;
+    }
+  }
+}
+</style>
