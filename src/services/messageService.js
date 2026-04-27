229db70ac28b088f22a65d81192f556df6a04cb2
yasmin <yalharasis@pando-soft.com>
2026-04-28 00:51:14 +0300

    add EmailJS to send email directly after send message from client

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,15 +1,28 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "@/firebase";
+import emailjs from '@emailjs/browser';
 
 export const sendMessage = async (form) => {
     try {
+        await emailjs.send(
+            import.meta.env.VITE_SERVICE_ID,
+            import.meta.env.VITE_TEMPLATE_ID,
+            {
+                name: form.name,
+                email: form.email,
+                subject: form.subject,
+                message: form.message,
+            },
+            import.meta.env.VITE_PUBLIC_KEY,
+        );
         await addDoc(collection(db, "messages"), {
             name: form.name,
             email: form.email,
             subject: form.subject,
             message: form.message,
-            createdAt: new Date,
+            createdAt: new Date(),
         });
+        console.log("Message + Email sent ✅");
         return { success: true };
     } catch (error) {
         console.error("Error", error);
